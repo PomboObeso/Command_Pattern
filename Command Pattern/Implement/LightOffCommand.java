eda030ab3081ebd934dbafa6e2d470b1442d46d6
PomboObeso <matheus.gomes201@gmail.com>
2020-01-31 23:02:11 -0300

    Implements Update

--- Command Pattern/Implement/LightOffCommand.java
+++ Command Pattern/Implement/LightOffCommand.java
@@ -3,9 +3,9 @@ class LightOffCommand implements Command {
     Light light;
 
     public LightOffCommand(Light light){
-        this.light = light;
+        this.light = light;                 //Comando que implementa Command
     }
     public void execute(){
-        light.off();
+        light.off();        //Metodo Invoker que executa o execute
     }
 }
