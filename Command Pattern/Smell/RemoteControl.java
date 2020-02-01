6b593521cef24ab0469b30c1ee9a5a709c9131a9
PomboObeso <matheus.gomes201@gmail.com>
2020-01-31 22:37:34 -0300

    Code Smell

---
+++ Command Pattern/Smell/RemoteControl.java
@@ -0,0 +1,30 @@
+public class RemoteControl {
+    
+    private GarageDoor garageDoor = new GarageDoor();
+    private Light light = new Light();
+
+    public RemoteControl(){}
+
+    public void execute (Command command){
+        if(command.equals(Command.UP)){
+            garageDoor.up();
+        } 
+        if(command.equals(Command.DOWN)){
+            garageDoor.down();
+        }
+        if(command.equals(Command.GARAGE_LIGHY_ON)) {
+            garageDoor.lightOn();
+        }
+        if(command.equals(Command.GARAGE_LIGHT_OFF)){
+            garageDoor.lightOff();
+        }
+        if(command.equals(Command.ON)){
+            light.on();
+        }
+        if(command.equals(Command.OFF)) {
+            light.off();
+        }
+
+
+    }
+}
